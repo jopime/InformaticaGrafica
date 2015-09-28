17c5a5bbd3c14410bb0a4d98ec265aaf80e551a5
jopime <jopime@correo.ugr.es>
2015-09-28 23:15:37 +0200

    Practicas Informatica Grafica
    Agregados los archivos iniciales de la Practica 1 sin modificar

---
+++ srcs-prac/practica1.cpp
@@ -0,0 +1,36 @@
+// *********************************************************************
+// **
+// ** Informática Gráfica, curso 2015-16
+// ** Práctica 1  (implementación)
+// **
+// *********************************************************************
+
+#include "aux.hpp"
+#include "tuplasg.hpp"   // Tupla3f 
+#include "practica1.hpp"
+
+// ---------------------------------------------------------------------
+// declaraciones de estructuras de datos....
+
+
+
+// ---------------------------------------------------------------------
+// Función para implementar en la práctica 1 para inicialización.
+// Se llama una vez al inicio, cuando ya se ha creado la ventana e 
+// incializado OpenGL. 
+
+void P1_Inicializar( int argc, char *argv[] )
+{
+   
+   
+}
+
+// ---------------------------------------------------------------------
+// Función a implementar en la práctica 1  para dibujar los objetos
+// modo: 0 - puntos, 1 - alambre, 2 - sólido, 3 - sólido ajedrez 
+
+void P1_DibujarObjetos( unsigned modo ) 
+{
+  
+
+}
---
+++ srcs-prac/practica1.hpp
@@ -0,0 +1,15 @@
+// *********************************************************************
+// **
+// ** Informática Gráfica, curso 2015-16
+// ** Práctica 1  (declaraciones públicas)
+// **
+// *********************************************************************
+
+#ifndef IG_PRACTICA1_HPP
+#define IG_PRACTICA1_HPP
+
+void P1_DibujarObjetos( unsigned modo ) ; 
+void P1_Inicializar( int argc, char *argv[] ) ;
+
+
+#endif
