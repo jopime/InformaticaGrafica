2abf51c6f86fd3cf59c9e5c5bbdc5ca99927a13d
jopime <jopime@correo.ugr.es>
2016-01-18 21:00:30 +0100

    borrado mis luces

--- srcs-prac/FuenteLuz.cpp
+++
@@ -1,6 +0,0 @@
-#include "FUENTELUZ.hpp"
-
-
-void FuenteLuz::activar(int i){
-  glEnable(GL_LIGHT0+i);
-}
--- srcs-prac/FuentePosicional.cpp
+++
@@ -1,8 +0,0 @@
-#include "FUENTEPOSICIONAL.hpp"
-
-FuentePosicional::FuentePosicional(const Tupla3f & posicion){
-  posvec=Tupla4f(posicion[0],posicion[1],posicion[2],1); //luz posicional w=1;
-  colores[0]= Tupla4f(0.3,0.3,0.3,1.0);
-  colores[1]= Tupla4f(0.3,0.3,0.3,1.0);
-  colores[2]= Tupla4f(0.3,0.3,0.3,1.0);
-}
